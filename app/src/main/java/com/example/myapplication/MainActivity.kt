85f8deae882faba81f34bce5ca868e934b4e1119
panji_pana <isabellewestcott24@gmail.com>
2022-09-07 14:42:21 +0100

    Initial push

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+        }
+    }
+
+
+}
